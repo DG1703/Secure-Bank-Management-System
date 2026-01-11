d20878c97d4cc849c76c66ea182be517dbaf3116
divya-garg <dgarg17306@gmail.com>
2026-01-11 18:12:07 +0530

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,1 +1,31 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
 
+class Account {
+private:
+    int accountNumber;
+    char name[50];
+    char password[20];
+    double balance;
+
+public:
+    // Account creation & authentication
+    void createAccount(int accNo);
+    bool authenticate(const char* pass) const;
+
+    // Transactions
+    void deposit(double amount);
+    bool withdraw(double amount);
+
+    // Display
+    void display() const;
+
+    // Getters
+    int getAccountNumber() const;
+    double getBalance() const;
+
+    // Setter (used internally by file logic)
+    void setBalance(double amount);
+};
+
+#endif
